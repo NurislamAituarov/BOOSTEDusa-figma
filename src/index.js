af2e5b1218a7f041bd3a2183231d9cbb1fe742ce
NurislamAituarov <nurislamajtuarov@gmail.com>
2021-12-09 22:35:06 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.scss';
+import './modules/scss/Header.scss';
+import './modules/scss/Main.scss';
+import './modules/scss/Accessories.scss';
+import './modules/scss/Footer.scss';
+console.log('Hello world');
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+// const isDev = process.env.NODE_ENV === 'development';
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  watch: true,
+};
