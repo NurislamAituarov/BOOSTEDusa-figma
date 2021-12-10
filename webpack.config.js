1414d2409d1d5f586e860a48f387d391541962c2
NurislamAituarov <nurislamajtuarov@gmail.com>
2021-12-10 13:36:37 +0600

    commit page

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,14 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   // watch: true,
